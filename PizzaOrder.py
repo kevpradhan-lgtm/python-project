074e65e47071b48db0208d1dd6deef909c3c46d4
kevpradhan-lgtm <kev.pradhan@gmail.com>
2025-11-15 12:17:33 -0500

    Create PizzaOrder.py

---
+++ PizzaOrder.py
@@ -0,0 +1,44 @@
+print("""
+                                                                                                            
+,-.----.                                                    ,----..                                         
+\    /  \                                                  /   /   \                                        
+|   :    \   ,--,                                         /   .     :              ,---,                    
+|   |  .\ :,--.'|          ,----,       ,----,           .   /   ;.  \  __  ,-.  ,---.'|            __  ,-. 
+.   :  |: ||  |,         .'   .`|     .'   .`|          .   ;   /  ` ;,' ,'/ /|  |   | :          ,' ,'/ /| 
+|   |   \ :`--'_      .'   .'  .'  .'   .'  .' ,--.--.  ;   |  ; \ ; |'  | |' |  |   | |   ,---.  '  | |' | 
+|   : .   /,' ,'|   ,---, '   ./ ,---, '   ./ /       \ |   :  | ; | '|  |   ,',--.__| |  /     \ |  |   ,' 
+;   | |`-' '  | |   ;   | .'  /  ;   | .'  / .--.  .-. |.   |  ' ' ' :'  :  / /   ,'   | /    /  |'  :  /   
+|   | ;    |  | :   `---' /  ;--,`---' /  ;--,\__\/: . .'   ;  \; /  ||  | ' .   '  /  |.    ' / ||  | '    
+:   ' |    '  : |__   /  /  / .`|  /  /  / .`|," .--.; | \   \  ',  / ;  : | '   ; |:  |'   ;   /|;  : |    
+:   : :    |  | '.'|./__;     .' ./__;     .'/  /  ,.  |  ;   :    /  |  , ; |   | '/  ''   |  / ||  , ;    
+|   | :    ;  :    ;;   |  .'    ;   |  .'  ;  :   .'   \  \   \ .'    ---'  |   :    :||   :    | ---'     
+`---'.|    |  ,   / `---'        `---'      |  ,     .-./   `---`             \   \  /   \   \  /           
+  `---`     ---`-'                           `--`---'                          `----'     `----'            
+                                                                                                            
+""")
+print("Welcome to python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ").upper()
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ").upper()
+extra_cheese = input("Do you want extra cheese? Y or N: ").upper()
+
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+else:
+    print("You typed the wrong input for size.")
+    exit()
+
+if pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is: ${bill}")
